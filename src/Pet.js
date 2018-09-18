ea59a3ea04cd0547a146e572df90a90d0186c176
Stanley Louis <stanlouis@msn.com>
2018-09-18 13:29:25 -0400

    Render a list of Pet components

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, media, location }) => {
+  let photos = [];
+  if (media && media.photos && media.photos.photo) {
+    photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h1>{animal}</h1>
-      <h1>{breed}</h1>
+    <div className="pet">
+      <div className="image-container">
+        <img src={photos[0].value} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
     </div>
   );
 };
--- src/app.js
+++ src/app.js
@@ -42,9 +42,26 @@ class App extends React.Component {
     return (
       <div>
         <h1>Adopt Me</h1>
-        <pre>
-          <code>{JSON.stringify(this.state, null, 4)}</code>
-        </pre>
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
