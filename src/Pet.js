7695d226145b9a3dce133ef1378ad0ccb076ad61
Stanley Louis <stanlouis@msn.com>
2018-09-18 11:28:42 -0400

    Initialize repo

---
+++ src/Pet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pet = ({ name, animal, breed }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>{animal}</h1>
+      <h1>{breed}</h1>
+    </div>
+  );
+};
+
+export default Pet;
---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me</h1>
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="Cat" breed="Mix" />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
